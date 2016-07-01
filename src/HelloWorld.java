2aed61194e2434b171ca7306e028d71a43f83529
SpudMillions <joshuaadams18@gmail.com>
2016-07-01 17:37:44 -0400

    First Java Project built in Eclipse and managed in Github

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+
+public class HelloWorld {
+
+	/**
+	 * @author	SpudMillions
+	 * @since	07/01/2016
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
